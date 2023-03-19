8950f6e1abc5bedc61c6863a3eaead9f760bf456
fdymendo <fdyrepositorios@gmail.com>
2023-03-19 18:19:57 -0500

    add decorator pattern

---
+++ src/main/java/com/fdymendo/learning/designpatterns/creational/decorator/model/Sugar.java
@@ -0,0 +1,18 @@
+package com.fdymendo.learning.designpatterns.creational.decorator.model;
+
+public class Sugar extends CoffeeDecorator {
+  public Sugar(Coffee coffee) {
+    super(coffee);
+  }
+
+  @Override
+  public double getCost() {
+    return super.getCost() + 0.2;
+  }
+
+  @Override
+  public String getDescription() {
+    return coffee.getDescription() + ", sugar" + " costs $" + coffee.getCost();
+  }
+
+}
