0b4df686e2f016da8524d03a8dda66a38822abce
FdyMendo <fdymendo@users.noreply.github.com>
2023-03-26 18:14:47 -0500

    add chain responsability (#12)
    Co-authored-by: fdymendo <fdyrepositorios@gmail.com>

---
+++ src/main/java/com/fdymendo/learning/designpatterns/creational/chainresponsability/controller/ChainResponsabilityController.java
@@ -0,0 +1,38 @@
+package com.fdymendo.learning.designpatterns.creational.chainresponsability.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.fdymendo.learning.designpatterns.creational.chainresponsability.model.Handler;
+import com.fdymendo.learning.designpatterns.creational.chainresponsability.model.HighValueHandler;
+import com.fdymendo.learning.designpatterns.creational.chainresponsability.model.LowValueHandler;
+import com.fdymendo.learning.designpatterns.creational.chainresponsability.model.Request;
+
+@RestController
+@RequestMapping("/ChainResponsability/v1")
+public class ChainResponsabilityController {
+
+  @GetMapping("/ValueLow")
+  public String getValueLow() {
+    Handler lowValueHandler = new LowValueHandler();
+    Handler highValueHandler = new HighValueHandler();
+
+    lowValueHandler.setNext(highValueHandler);
+
+    Request request1 = new Request("Request with a low value", 50);
+
+    return lowValueHandler.handle(request1);
+  }
+
+  @GetMapping("/ValueHigh")
+  public String getValueHigh() {
+    Handler lowValueHandler = new LowValueHandler();
+    Handler highValueHandler = new HighValueHandler();
+
+    lowValueHandler.setNext(highValueHandler);
+
+    Request request2 = new Request("Request with a high value", 150);
+
+    return lowValueHandler.handle(request2);
+  }
+}
