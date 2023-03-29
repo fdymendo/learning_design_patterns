f315e1a0590188392c9ae531367e13779e70ec85
FdyMendo <fdymendo@users.noreply.github.com>
2023-03-29 12:47:29 -0500

    add command (#13)

---
+++ src/main/java/com/fdymendo/learning/designpatterns/creational/command/controller/CommandController.java
@@ -0,0 +1,34 @@
+package com.fdymendo.learning.designpatterns.creational.command.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.fdymendo.learning.designpatterns.creational.command.model.Light;
+import com.fdymendo.learning.designpatterns.creational.command.model.LightOnCommand;
+import com.fdymendo.learning.designpatterns.creational.command.model.RemoteControl;
+
+@RestController
+@RequestMapping("/Command/v1")
+public class CommandController {
+
+
+  @GetMapping("/Execute")
+  public String executeButton() {
+    Light light = new Light();
+    LightOnCommand lightOnCommand = new LightOnCommand(light);
+    RemoteControl remote = new RemoteControl();
+    remote.setCommand(lightOnCommand);
+
+    return remote.pressButton(); // Light turned on.
+  }
+  
+  @GetMapping("/Undo")
+  public String undoButton() {
+    Light light = new Light();
+    LightOnCommand lightOnCommand = new LightOnCommand(light);
+    RemoteControl remote = new RemoteControl();
+    remote.setCommand(lightOnCommand);
+
+    return remote.pressUndoButton(); // Light turned on.
+  }
+}
