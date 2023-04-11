422687d27849a0ac901a4785ba59967a5a90d760
FdyMendo <fdymendo@users.noreply.github.com>
2023-04-10 19:09:13 -0500

    Add strategy (#22)

---
+++ src/main/java/com/fdymendo/learning/designpatterns/behavioral/strategy/model/Item.java
@@ -0,0 +1,11 @@
+package com.fdymendo.learning.designpatterns.behavioral.strategy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Item {
+  private String name;
+  private double price;
+}
